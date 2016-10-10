aa282ff2849b65bfe9a1baa8b2fbcbbf402952b5
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-10 09:15:29 +0200

    Created main Java package and package-info.

---
+++ src/fr/insalyon/if4/h4203/pldagile/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * @brief Main package for the application
+ * 
+ * This is the main package for the application developped during "Agile" long-term project in 4th year of INSA Lyon Computer Engineering department.
+ * 
+ * This package will only contain the subpackages of the application.
+ * 
+ * 
+ * @authors Thibaut FERNANDEZ
+ * @authors Pierre-Louis LEFEBVRE
+ * @authors Fada MESSAOUDI
+ * @authors Joachim DOREL
+ * @authors Victor CAPELLE
+ * @authors Victoire CHAPELLE
+ * @authors Eric JORDAN
+ *
+ */
+package fr.insalyon.if4.h4203.pldagile;
