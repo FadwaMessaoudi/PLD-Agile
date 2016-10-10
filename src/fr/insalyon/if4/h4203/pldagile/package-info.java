9a366f083138de3dfed6bae0faab07edeeb99e77
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-10 10:53:43 +0200

    Began writing Doxygen main page

--- src/fr/insalyon/if4/h4203/pldagile/package-info.java
+++ src/fr/insalyon/if4/h4203/pldagile/package-info.java
@@ -3,7 +3,7 @@
  * 
  * This is the main package for the application developped during "Agile" long-term project in 4th year of INSA Lyon Computer Engineering department.
  * 
- * This package will only contain the subpackages of the application.
+ * This package will only contain the subpackages of the application, which will themselves contain the Java code.
  * 
  * 
  * @authors Thibaut FERNANDEZ
@@ -16,3 +16,14 @@
  *
  */
 package fr.insalyon.if4.h4203.pldagile;
+
+/** @mainpage PLD Agile - INSA Lyon 4IF H4203
+ * 
+ * @section intro Presentation
+ * 
+ * This is the Doxygen documentation for the application created during long-term project "PLD AGILE" during 4th year at INSA Lyon, Computer Engineering Department.
+ * 
+ * The goal of this project is to give students experience in using agile development methods. To reach this goal, students had to develop a delivery drive optimization software.
+ * 
+ * 
+ */
