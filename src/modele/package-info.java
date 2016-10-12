2b3bd9f95f1cb1495e3617b4f1ee1dce3d6c8f78
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-12 11:09:43 +0200

    Doc for Model package

--- src/modele/package-info.java
+++ src/modele/package-info.java
@@ -1,8 +1,11 @@
+
 /**
+ * @brief Data structures
  * 
- */
-/**
- * @author Sam
+ * This package contains the data structure for the application.
+ * They consist of classes directly representing elements from the data model.
+ * 
+ * @author Victor CAPELLE
  *
  */
 package modele;
