a956805ffe4cc16a0ec082186f60c8d7c8fd0be2
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-12 11:31:34 +0200

    Created Controller class skeleton

---
+++ src/fr/insalyon/if4/h4203/pldagile/controller/Controller.java
@@ -0,0 +1,70 @@
+package fr.insalyon.if4.h4203.pldagile.controller;
+
+/**
+ * @brief State-machine Controller
+ * 
+ * This is the controller for our implementation
+ * of the State/Controller design pattern.
+ * 
+ * The purpose of this class is to manage the workflow
+ * and the window progression of the application.
+ * 
+ * @author Thibaut FERNANDEZ
+ *
+ */
+public class Controller {
+
+	/**
+	 * @brief Default constructor for the controller
+	 * 
+	 * @toimplement
+	 */
+	public Controller() {
+		// TODO
+	}
+
+	/**
+	 * @brief Command to load an XML file
+	 * 
+	 * @toimplement
+	 */
+	public void load(String uri) {
+		//TODO
+	}
+
+	/**
+	 * @brief User acknowledges a message
+	 * 
+	 * @toimplement
+	 */
+	public void acknowledge() {
+		//TODO
+	}
+
+	/**
+	 * @brief User wants to load a map
+	 * 
+	 * @toimplement
+	 */
+	public void loadStreetMap() {
+		//TODO
+	}
+
+	/**
+	 * @brief User wants to load a delivery order
+	 * 
+	 * @toimplement
+	 */
+	public void loadOrder() {
+		//TODO
+	}
+
+	/**
+	 * @brief User wants to compute a delivery round
+	 * 
+	 * @toimplement
+	 */
+	public void computeRound() {
+		//TODO
+	}
+}
