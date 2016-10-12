e2b31df17fe93424f60d73e9c8e8436dd9581aa2
vchapelle <victoire.chapelle@gmail.com>
2016-10-12 11:46:26 +0200

    Modification of Deserializer

--- src/xml/Deserializer.java
+++ src/xml/Deserializer.java
@@ -7,6 +7,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import modele.Delivery;
+import modele.DeliveryOrder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -23,8 +26,8 @@ public class Deserializer {
         /*if (racine.getNodeName().equals("reseau")) {
            construireAPartirDeDOMXML(racine, plan);
         }
-        else if(racine.getNodeName().equals("demandeDeLivraison"))
-        
+        else if(racine.getNodeName().equals("demandeDeLivraisons"))
+        	construireAPartirDeDOMXML(racine, deliveryOrder);
         }else
         	throw new ExceptionXML("Document non conforme");
         */
@@ -40,12 +43,30 @@ public class Deserializer {
        	plan.reset(largeur,hauteur);
        	NodeList listeCercles = noeudDOMRacine.getElementsByTagName("cercle");
        	for (int i = 0; i < listeCercles.getLength(); i++) {
-        	plan.ajoute(creeCercle((Element) listeCercles.item(i)));
+        	//plan.ajoute(creeCercle((Element) listeCercles.item(i)));
        	}
        	NodeList listeRectangles = noeudDOMRacine.getElementsByTagName("rectangle");
        	for (int i = 0; i < listeRectangles.getLength(); i++) {
-          	plan.ajoute(creeRectangle((Element) listeRectangles.item(i)));
+          	//plan.ajoute(creeRectangle((Element) listeRectangles.item(i)));
        	}
+    }
+    
+    private static void construireAPartirDeDOMXML(Element noeudDOMRacine, DeliveryOrder deliveryOrder) throws ExceptionXML, NumberFormatException{
+    	int warehouseAddress = Integer.parseInt(((Element)noeudDOMRacine.getElementsByTagName("entrepot")).getAttribute("adresse"));
+        /*if (entrepot <= 0)
+        	throw new ExceptionXML("Erreur lors de la lecture du fichier : La hauteur du plan doit etre positive");*/
+        
+       	NodeList listDelivery = noeudDOMRacine.getElementsByTagName("livraison");
+       	for (int i = 0; i < listDelivery.getLength(); i++) {
+        	deliveryOrder.add(createDelivery((Element) listDelivery.item(i)));
+       	}	
+    }
+    
+    private static Delivery createDelivery(Element elt) throws ExceptionXML{
+    	int address = Integer.parseInt(elt.getAttribute("adresse"));
+   		int duration = Integer.parseInt(elt.getAttribute("duree"));
+   		
+   		return new Delivery(address, duration);
     }
  
 }
