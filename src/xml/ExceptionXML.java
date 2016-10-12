8903135e84187b30f46c2a2d2f510b8db410a987
vchapelle <victoire.chapelle@gmail.com>
2016-10-12 09:36:15 +0200

    Creation of the xml package

---
+++ src/xml/ExceptionXML.java
@@ -0,0 +1,14 @@
+package xml;
+
+public class ExceptionXML extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7981113938213427813L;
+
+	public ExceptionXML(String message) {
+		super(message);
+	}
+
+}
