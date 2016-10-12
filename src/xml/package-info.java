135831155f358e144d647c00f20d6d9c9c3b5c49
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-12 11:12:46 +0200

    Package doc for XML package

---
+++ src/xml/package-info.java
@@ -0,0 +1,12 @@
+
+/**
+ * @brief XML reading and parsing tools
+ * 
+ * This package contains the tools used to open and parse XML files.
+ * These tools generate 
+ * 
+ * @authors Victoire CHAPELLE
+ * @authors Joachim DOREL
+ * 
+ */
+package xml;
