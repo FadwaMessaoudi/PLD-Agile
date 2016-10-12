a2af188653732a2a5cd13c6e0e7f111aa6e1b51f
capellev <capellev.info@gmail.com>
2016-10-12 10:54:56 +0200

    Modele

---
+++ src/modele/Delivery.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Delivery {
+	private int deliveryDuration;
+	private Intersection address;
+	
+	Delivery(Intersection address, int deliveryDuration) {
+		this.address = address;
+		this.deliveryDuration = deliveryDuration;
+	}
+	
+	int getDeliveryDuration() {
+		return deliveryDuration;
+	}
+}
---
+++ src/modele/Intersection.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Intersection {
+	private int x;
+	private int y;
+	
+	Intersection(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
