705a729c6c66885244c3063cf440702e66a8b6cc
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-17 09:31:43 +0200

    Started working on street map loading states

--- src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/LoadStreetMapState.java
+++ src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/LoadStreetMapState.java
@@ -2,16 +2,32 @@ package fr.insalyon.if4.h4203.pldagile.controller.states.loadstreetmap;
 
 import fr.insalyon.if4.h4203.pldagile.controller.Controller;
 import fr.insalyon.if4.h4203.pldagile.controller.states.DefaultState;
+import fr.insalyon.if4.h4203.pldagile.controller.states.StatesEnum;
 
+/**
+ * @brief Loading a street map file
+ * 
+ * This state is active when the application is loading a street map file.
+ * This state should be the startup state of the application.
+ * 
+ * @author Thibaut FERNANDEZ
+ *
+ */
 public class LoadStreetMapState extends DefaultState {
 
+	/// @brief Reference to the application controller
 	private final Controller controller;
 	
 	public LoadStreetMapState(Controller c) {
+		this.controller = c;
 		
 	}
 	
 	public void loadStreetMap() {
-		
+		this.controller.setState(StatesEnum.PROMPT_FOR_MAP_STATE);
+	}
+	
+	public void loadOrder() {
+		this.controller.setState(StatesEnum.PROMPT_FOR_ORDER_STATE);
 	}
 }
--- src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/PromptForMapState.java
+++ src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/PromptForMapState.java
@@ -5,9 +5,22 @@ import fr.insalyon.if4.h4203.pldagile.controller.states.DefaultState;
 
 public class PromptForMapState extends DefaultState {
 
+	/// @brief Reference to the application controller
 	private final Controller controller;
-	
+
+	/**
+	 * @brief Constructor for this state class
+	 * 
+	 * This constructor performs the following actions, which are entry actions for this state:
+	 * 
+	 * @li Ask for the location of an xml file
+	 * @li Attempt to parse the file and convert it to an OMap
+	 * @li If parsing fails, change the state to InvalidStreetMapState
+	 * @li If parsing suceeds, change the state to LoadStreetMapState
+	 * 
+	 */
 	public PromptForMapState(Controller c) {
+		this.controller = c;
 		
 	}
 	
