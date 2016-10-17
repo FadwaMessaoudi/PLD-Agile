633592e8344ce0eed288f5e7f54aa077ee00eff9
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-17 08:40:37 +0200

    Created state class stubs for LoadDeliveryOrder package

---
+++ src/fr/insalyon/if4/h4203/pldagile/controller/states/loaddeliveryorder/InvalidOrderState.java
@@ -0,0 +1,17 @@
+package fr.insalyon.if4.h4203.pldagile.controller.states.loaddeliveryorder;
+
+import fr.insalyon.if4.h4203.pldagile.controller.Controller;
+import fr.insalyon.if4.h4203.pldagile.controller.states.DefaultState;
+
+public class InvalidOrderState extends DefaultState {
+
+	private final Controller controller;
+	
+	public InvalidOrderState(Controller c) {
+		
+	}
+	
+	public void acknowledge() {
+		
+	}
+}
