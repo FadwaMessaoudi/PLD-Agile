5f75f960e447b54d62592eddd64534a71bc4fc54
Nathan Shiraini <thibaut.fernandez@insa-lyon.fr>
2016-10-17 08:50:51 +0200

    Created state class stubs for LoadStreetMap and RoundManagement.

---
+++ src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/InvalidStreetMapState.java
@@ -0,0 +1,17 @@
+package fr.insalyon.if4.h4203.pldagile.controller.states.loadstreetmap;
+
+import fr.insalyon.if4.h4203.pldagile.controller.Controller;
+import fr.insalyon.if4.h4203.pldagile.controller.states.DefaultState;
+
+public class InvalidStreetMapState extends DefaultState {
+
+	private final Controller controller;
+	
+	public InvalidStreetMapState(Controller c) {
+		
+	}
+	
+	public void acknowledge() {
+		
+	}
+}
---
+++ src/fr/insalyon/if4/h4203/pldagile/controller/states/loadstreetmap/PromptForMapState.java
@@ -0,0 +1,17 @@
+package fr.insalyon.if4.h4203.pldagile.controller.states.loadstreetmap;
+
+import fr.insalyon.if4.h4203.pldagile.controller.Controller;
+import fr.insalyon.if4.h4203.pldagile.controller.states.DefaultState;
+
+public class PromptForMapState extends DefaultState {
+
+	private final Controller controller;
+	
+	public PromptForMapState(Controller c) {
+		
+	}
+	
+	public void load(String uri) {
+		
+	}
+}
