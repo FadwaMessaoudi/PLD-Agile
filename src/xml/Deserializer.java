8032f3610460755d1e07b174c0ec3cf07b8da495
vchapelle <victoire.chapelle@gmail.com>
2016-10-17 09:22:27 +0200

    work on deserializer

--- src/xml/Deserializer.java
+++ src/xml/Deserializer.java
@@ -7,8 +7,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import modele.Delivery;
-import modele.DeliveryOrder;
 import modele.OMap;
 
 import org.w3c.dom.Document;
@@ -19,50 +17,55 @@ import org.xml.sax.SAXException;
 
 public class Deserializer {
 	
-	public static void load() throws ParserConfigurationException, SAXException, IOException, ExceptionXML{
+	public static void load(OMap omap) throws ParserConfigurationException, SAXException, IOException, ExceptionXML{
 		File xml = XMLFileOpener.getInstance().open(true);
         DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();	
         Document document = docBuilder.parse(xml);
         Element racine = document.getDocumentElement();
-        if (racine.getNodeName().equals("reseau")) {
-        	//buildMap(racine);
-        }
-        else if(racine.getNodeName().equals("demandeDeLivraisons"))
-        	buildDeliveryOrder(racine);
-        }/*else{
+        
+        if (racine.getNodeName().equals("reseau")) 
+        	buildMap(racine,omap);
+        
+        else if (racine.getNodeName().equals("demandeDeLivraisons")) 
+        	buildDeliveryOrder(racine,omap);
+        else
         	throw new ExceptionXML("Document non conforme");
-        }*/
+        }
+	
 
-    private static void buildMap(Element noeudDOMRacine, OMap omap) throws ExceptionXML, NumberFormatException{
-    	int hauteur = Integer.parseInt(noeudDOMRacine.getAttribute("hauteur"));
-        if (hauteur <= 0)
-        	throw new ExceptionXML("Erreur lors de la lecture du fichier : La hauteur du plan doit etre positive");
-        int largeur = Integer.parseInt(noeudDOMRacine.getAttribute("largeur"));
-        if (largeur <= 0)
-        	throw new ExceptionXML("Erreur lors de la lecture du fichier : La largeur du plan doit etre positive");
-       	//omap.reset(largeur,hauteur);
-       	NodeList listeCercles = noeudDOMRacine.getElementsByTagName("cercle");
-       	for (int i = 0; i < listeCercles.getLength(); i++) {
-        	//plan.ajoute(creeCercle((Element) listeCercles.item(i)));
+    private static void buildMap(Element nodeDOMRacine, OMap omap) throws ExceptionXML, NumberFormatException{
+    	
+       	NodeList intersectionList = nodeDOMRacine.getElementsByTagName("noeud");
+       	for (int i = 0; i < intersectionList.getLength(); i++) {
+       		int id = Integer.parseInt(((Element)intersectionList).getAttribute("id"));
+       		int x = Integer.parseInt(((Element)intersectionList).getAttribute("x"));
+       		int y = Integer.parseInt(((Element)intersectionList).getAttribute("y"));
+        	omap.addIntersection(x,y,id);
        	}
-       	NodeList listeRectangles = noeudDOMRacine.getElementsByTagName("rectangle");
-       	for (int i = 0; i < listeRectangles.getLength(); i++) {
-          	//plan.ajoute(creeRectangle((Element) listeRectangles.item(i)));
+       	
+       	NodeList sectionList = nodeDOMRacine.getElementsByTagName("troncon");
+       	for (int i = 0; i < sectionList.getLength(); i++) {
+       		int start = Integer.parseInt(((Element)sectionList).getAttribute("origine"));
+       		int end = Integer.parseInt(((Element)sectionList).getAttribute("destination"));
+       		int length = Integer.parseInt(((Element)sectionList).getAttribute("longueur"));
+       		int averageSpeed = Integer.parseInt(((Element)sectionList).getAttribute("vitesse"));
+       		String name = (String)((Element)sectionList).getAttribute("nomRue");
+          	omap.addSection(start, end, length, averageSpeed, name);
        	}
     }
     
-    private static void buildDeliveryOrder(Element noeudDOMRacine) throws ExceptionXML, NumberFormatException{
-    	int warehouseAddress = Integer.parseInt(((Element)noeudDOMRacine.getElementsByTagName("entrepot")).getAttribute("adresse"));
-        //String warehouseDepartureTime = (String)((Element)noeudDOMRacine.getElementsByTagName("entrepot")).getAttribute("adresse");
+    private static void buildDeliveryOrder(Element nodeDOMRacine, OMap omap) throws ExceptionXML, NumberFormatException{
+    	int warehouseAddress = Integer.parseInt(((Element)nodeDOMRacine.getElementsByTagName("entrepot")).getAttribute("adresse"));
+        //String warehouseDepartureTime = (String)((Element)nodeDOMRacine.getElementsByTagName("entrepot")).getAttribute("adresse");
         
-       	NodeList listDelivery = noeudDOMRacine.getElementsByTagName("livraison");
+       	NodeList listDelivery = nodeDOMRacine.getElementsByTagName("livraison");
        	int listDeliveries [][] = new int [listDelivery.getLength()][2];
        	for (int i = 0; i < listDelivery.getLength(); i++) {
        		listDeliveries[i][0] = Integer.parseInt(((Element)listDelivery).getAttribute("adresse"));
        		listDeliveries[i][1] = Integer.parseInt(((Element)listDelivery).getAttribute("duree"));
        	}
        	//Send to DeliveryOrder warehouse's address and the deliveries
-       	//DeliveryOrder.create(warehouseAddress,listDeliveries);
+       	omap.buildeDeliveryOrder(warehouseAddress,listDeliveries);
        	
        	
        	
